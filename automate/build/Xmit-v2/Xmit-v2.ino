c0e537c26ef690a0080e17dd0b4918e012f7368d
benpro95 <benprovenzano@gmail.com>
2021-06-23 14:49:44 -0400

    Updated: 6/23/21

--- automate/build/Xmit-v2/Xmit-v2.ino
+++ automate/build/Xmit-v2/Xmit-v2.ino
@@ -3,30 +3,38 @@
 
 #include <string.h>  
 
-// IR Out Pin #2 on Uno / Pin #13 on Micro
+// IR Library
+// Output pin #2 on Uno, pin #13 on Micro
 #include <IRremote.h>
 IRsend irsend;
 
+// RF Library
 #include <RCSwitch.h>
 RCSwitch mySwitch = RCSwitch();
 
 String readString;
 
 void setup() {
+  // Serial Port
   Serial.begin(9600);
+  
   // Built-in LED
   pinMode(13, OUTPUT); 
   digitalWrite(13, LOW);
-  // FET-2 Pin #5 (Internal Fan)
+  
+  // FET-2 (Internal Fan)
   pinMode(5, OUTPUT);
   digitalWrite(5,LOW);
-  // FET-0 Pin #6 (Main Light)
+  
+  // FET-0 (Main Light)
   pinMode(6, OUTPUT);
   digitalWrite(6,LOW);
-  // FET-1 Pin #7 (Network Switch)
+  
+  // FET-1 (Network Switch)
   pinMode(7, OUTPUT);
   digitalWrite(7,HIGH);
-  // 433MHz Transmitter Pin #10
+  
+  // RF transmit output on pin #10
   mySwitch.enableTransmit(10);
   mySwitch.setPulseLength(183);
   mySwitch.setProtocol(1);
@@ -48,14 +56,14 @@ void loop() {
 // IR Transmit (must be in integer format)
 if(readString.indexOf("irtx.") >=0)
 {
-  Serial.print("IR Transmit\n");
+  //Serial.print("IR Transmit\n");
   if(readString.indexOf("nec.") >=4)
   {
     // NEC IR Transmit 32-bit
     String ircode = readString.substring(9,27);
-    Serial.println("NEC");
-    Serial.println(ircode);
-    Serial.println("\n");
+    //Serial.println("NEC");
+    //Serial.println(ircode);
+    //Serial.println("\n");
     irsend.sendNEC(ircode.toInt(), 32);
     delay(30);
   }
@@ -63,9 +71,9 @@ if(readString.indexOf("irtx.") >=0)
   {
     // SONY IR Transmit 20-bit
     String ircode = readString.substring(12,30);
-    Serial.println("Sony 20-bit");
-    Serial.println(ircode);
-    Serial.println("\n");
+    //Serial.println("Sony 20-bit");
+    //Serial.println(ircode);
+    //Serial.println("\n");
     irsend.sendSony(ircode.toInt(), 20);
     delay(30);
   }
@@ -73,9 +81,9 @@ if(readString.indexOf("irtx.") >=0)
   {
     // SONY IR Transmit 12-bit
     String ircode = readString.substring(12,30);
-    Serial.println("Sony 12-bit");
-    Serial.println(ircode);
-    Serial.println("\n");
+    //Serial.println("Sony 12-bit");
+    //Serial.println(ircode);
+    //Serial.println("\n");
     irsend.sendSony(ircode.toInt(), 12);
     delay(30);
   }
@@ -84,14 +92,14 @@ if(readString.indexOf("irtx.") >=0)
 // FET Control
 if(readString.indexOf("fet.") >=0)
 {
-  Serial.print("FET Control\n");
+  //Serial.print("FET Control\n");
   if(readString.indexOf("on.") >=3)
   {
     // FET on
     String fet = readString.substring(7,9);
-    Serial.println("FET on");
-    Serial.println(fet);
-    Serial.println("\n");
+    //Serial.println("FET on");
+    //Serial.println(fet);
+    //Serial.println("\n");
     digitalWrite(fet.toInt(), HIGH);
     delay(30);
   }
@@ -99,9 +107,9 @@ if(readString.indexOf("fet.") >=0)
   {
     // FET off
     String fet = readString.substring(8,10);
-    Serial.println("FET off");
-    Serial.println(fet);
-    Serial.println("\n");
+    //Serial.println("FET off");
+    //Serial.println(fet);
+    //Serial.println("\n");
     digitalWrite(fet.toInt(), LOW);
     delay(30);
   }
@@ -111,9 +119,9 @@ if(readString.indexOf("fet.") >=0)
 if(readString.indexOf("rftx.") >=0)
 {
   String rfcode = readString.substring(5,20);
-  Serial.println("RF Transmit");
-  Serial.println(rfcode);
-  Serial.println("\n");
+  //Serial.println("RF Transmit");
+  //Serial.println(rfcode);
+  //Serial.println("\n");
   mySwitch.send(rfcode.toInt(), 24);
   delay(30);
 }

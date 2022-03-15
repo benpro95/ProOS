be2e29e4d99ab26f1e376e874d59e050910893d9
Ben Provenzano III <benprovenzano@gmail.com>
2022-03-14 21:09:08 -0400

    2022-03-14 21:09

--- wkst/Scripts/extbkp.ps1
+++ wkst/Scripts/extbkp.ps1
@@ -1,14 +1,12 @@
 ## Backup Server to External NTFS Drives
 
-## 128GB Drives Excluded Data 
-$Bkp128_Exc='"\\files\Ben\ProOS\.git" "\\files\Ben\ProOS\pve" "\\files\Ben\Software\"'
-
 ## 128GB Flash Drive I
 $Bkp1_Label="Bkp128-I"
 $Bkp1_Letter = Get-Volume -FileSystemLabel "$Bkp1_Label" | % DriveLetter
 if (Test-Path -Path "${Bkp1_Letter}:\Ben"){
   echo "Starting backup on $Bkp1_Label..."
-  robocopy \\files\Ben ${Bkp1_Letter}:\Ben /MIR /XJD /FFT /R:3 /W:10 /Z /XD $Bkp128_Exc
+  robocopy \\files\Ben ${Bkp1_Letter}:\Ben /MIR /XJD /FFT /R:3 /W:10 /Z /XD `
+    "\\files\Ben\ProOS\.ssh" "\\files\Ben\ProOS\.git" "\\files\Ben\ProOS\pve\vmbkps" "\\files\Ben\Software"
 }else{
   echo "$Bkp1_Label not connected."
 }
@@ -19,7 +17,8 @@ $Bkp2_Label="Bkp128-II"
 $Bkp2_Letter = Get-Volume -FileSystemLabel "$Bkp2_Label" | % DriveLetter
 if (Test-Path -Path "${Bkp2_Letter}:\Ben"){
   echo "Starting backup on $Bkp2_Label..."
-  robocopy \\files\Ben ${Bkp2_Letter}:\Ben /MIR /XJD /FFT /R:3 /W:10 /Z /XD $Bkp128_Exc
+  robocopy \\files\Ben ${Bkp2_Letter}:\Ben /MIR /XJD /FFT /R:3 /W:10 /Z /XD `
+    "\\files\Ben\ProOS\.ssh" "\\files\Ben\ProOS\.git" "\\files\Ben\ProOS\pve\vmbkps" "\\files\Ben\Software"
 }else{
   echo "$Bkp2_Label not connected."
 }
@@ -27,12 +26,12 @@ echo ""
 
 ## 4TB Western Digital Red
 $Bkp3_Label="Bkp4TBRed"
-$Bkp3_Exc='"\\files\Ben\ProOS\.ssh" "\\files\Ben\ProOS\.git"'
 $Bkp3_Letter = Get-Volume -FileSystemLabel "$Bkp3_Label" | % DriveLetter
 if (Test-Path -Path "${Bkp3_Letter}:\Ben"){
   echo "Starting backup on $Bkp3_Label..."
   robocopy \\files\Media ${Bkp3_Letter}:\Media /MIR /XJD /FFT /R:3 /W:10 /Z
-  robocopy \\files\Ben ${Bkp3_Letter}:\Ben /MIR /XJD /FFT /R:3 /W:10 /Z /XD $Bkp3_Exc
+  robocopy \\files\Ben ${Bkp3_Letter}:\Ben /MIR /XJD /FFT /R:3 /W:10 /Z /XD `
+    "\\files\Ben\ProOS\.ssh" "\\files\Ben\ProOS\.git"
 }else{
   echo "$Bkp3_Label not connected."
 }

6831af5fbdb8b588f0a5d9b308655005a64a4850
Ben Provenzano III <benprovenzano@gmail.com>
2022-11-16 11:40:55 -0500

    2022-11-16 11:40

--- pve/automate/config/html/main.js
+++ pve/automate/config/html/main.js
@@ -1,10 +1,13 @@
 function sendCmd(act, arg1, arg2) {
-	let url = "http://"+location.hostname+":9300/exec.php?var="+arg2+"&arg="+arg1+"&action="+act;
+	let url = "http://"+location.hostname+"/exec.php?var="+arg2+"&arg="+arg1+"&action="+act;
 	document.getElementById("bottom").innerHTML = url;
 	fetch(url, {
-    method: 'GET',
-  })
-}
+      method: 'GET',
+    })
+ }
+
+
 function GoToHomePage() {
   window.location = '/';   
 }
+

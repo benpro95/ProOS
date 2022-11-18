303b95944d9738cda79bb23e51a6b1d91dfa573e
Ben Provenzano III <benprovenzano@gmail.com>
2022-11-18 09:37:12 -0500

    2022-11-18 09:37

--- pve/automate/config/html/main.js
+++ pve/automate/config/html/main.js
@@ -1,19 +1,66 @@
+// globals
+var vol_mode = 1;
+var load_bar = 0;
+
+// on-page-load
+window.onload = function() {
+	volMode();
+  var elem = document.getElementById("load__bar");
+  elem.textContent = "Automate";  
+};
+
+// transmit command
 function sendCmd(act, arg1, arg2) {
+	if (vol_mode == 1) {  
+      arg2 = "subs"
+    }
 	let url = "http://"+location.hostname+"/exec.php?var="+arg2+"&arg="+arg1+"&action="+act;
 	document.getElementById("bottom").innerHTML = url;
 	fetch(url, {
       method: 'GET',
     })
- }
+  loadBar();
+};
 
+// back to home page 
 function GoToHomePage() {
   window.location = '/';   
+};
+
+function volMode() {   
+	let id = document.getElementById("sub__text");
+    if (vol_mode == 0) {
+       id.textContent = "Subs";
+       vol_mode = 1;
+    } else {	
+       id.textContent = "Bedroom";
+       vol_mode = 0;
+    }
+};
+
+function disableButton() {
+	document.getElementById("sub__text").disabled = true;
+};	
+
+function loadBar() {
+  if (load_bar == 0) {
+    load_bar = 1;
+    var elem = document.getElementById("load__bar");
+    elem.textContent = " ";  
+    var width = 1;
+    var id = setInterval(frame, 0.5);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        load_bar = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+      if (width >= 100) {
+        elem.style.width = 0;  
+        elem.textContent = "Automate";  
+      }
+    }
+  }
 }
-
-function subLock() {
-    let id = document.getElementById("LockButton");
-    id.classList.remove("fa-lock");
-    id.classList.add("fa-user");
-}
-
-

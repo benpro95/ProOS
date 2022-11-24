11ed5a64dfb3af14ea9088dc342b8a4a5b0070c2
Ben Provenzano III <benprovenzano@gmail.com>
2022-11-23 22:39:34 -0500

    2022-11-23 22:39

--- pve/files/config/wwwrun.sh
+++ pve/files/config/wwwrun.sh
@@ -6,8 +6,11 @@ REPLY="$1"
 ARG="$2"
 LOGFILE="/mnt/ramdisk/sysout.txt"
 
+echo ""
 HEADER=$(date)
+HEADER+=" ("
 HEADER+=$(hostname)
+HEADER+=")"
 echo "$HEADER" &>> $LOGFILE
 /usr/bin/www.sh $REPLY $ARG $LOGFILE &>> $LOGFILE
 echo "" &>> $LOGFILE

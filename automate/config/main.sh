3667087def2b5af302f613935dc2a5be0f7df3f4
Ben Provenzano III <benprovenzano@gmail.com>
2023-09-02 21:46:54 -0400

    2023-09-02 21:46

--- automate/config/main.sh
+++ automate/config/main.sh
@@ -704,7 +704,7 @@ _CMDARG=${CMDARG//$'\n'/}
 SERVERARG=${_CMDARG%-*}
 FILESCMD=${_CMDARG#*-}
 ## LCDpi message
-LCDPI_MSG="($_CMDARG) command sent"
+LCDPI_MSG="($SERVERARG) command sent"
 CALL_LCDPI
 ## transmit action to file server
 if [ "$SERVERARG" == "files" ]; then

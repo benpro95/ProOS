34016c6b9df69af5c53401636a707a570143ab12
Ben Provenzano III <benprovenzano@gmail.com>
2023-11-27 19:25:16 -0500

    2023-11-27 19:25

--- rpi/effects/patterns/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import modifiers
-
-from .fade import Fade
-from .scroll import Scroll
-from .solid_color import SolidColor
-from .stripes import Stripes
-from .off import Off
-
-__all__ = ['Fade', 'Scroll', 'SolidColor', 'Stripes', 'Off']
--- rpi/effects/patterns/modifiers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This package contains patterns which are modifiers for existing patterns. If
-a pattern takes another pattern in its constructor, it belongs in this package.
-"""
-
-from .strobe import Strobe
-
-__all__ = ['Strobe']

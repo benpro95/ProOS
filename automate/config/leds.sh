cf59adfe53c7258695cf403e23364058976a6657
Ben Provenzano III <benprovenzano@gmail.com>
2024-01-10 21:38:18 -0500

    2024-01-10 21:38

--- automate/config/leds.sh
+++ automate/config/leds.sh
@@ -4,6 +4,18 @@
 ###########################################################
 
 CALL_LCDPI(){
+  ## Send message to LCDpi
+  if [ "$VARA" == "fc" ]; then
+    LCDPI_MSG="$VARB% brightness."
+  else
+    LCDPI_MSG="$VARA."
+    if [ "$VARA" == "stop" ]; then
+      LCDPI_MSG="stop LEDs."
+    fi
+    if [ "$VARA" == "pause" ]; then
+      LCDPI_MSG="pause LEDs."
+    fi
+  fi
   /usr/bin/curl -silent --fail --ipv4 --no-buffer \
      --max-time 5 --retry 1 --no-keepalive \
      -X POST http://lcdpi.home/upload.php \
@@ -20,19 +32,7 @@ if [ -e /opt/system/ledsync.txt ]; then
     /usr/bin/curl --silent --fail --ipv4 --no-buffer \
      --max-time 5 --retry 1 --no-keepalive \
      --data "var=$VARB&arg=$VARA&action=leds" http://%/exec.php
-  ## Send message to LCDpi
-  if [ "$VARA" == "fc" ]; then
-    LCDPI_MSG="$VARB% brightness."
-  else
-   	LCDPI_MSG="$VARA."
-    if [ "$VARA" == "stop" ]; then
-      LCDPI_MSG="stop LEDs."
-    fi
-    if [ "$VARA" == "pause" ]; then
-      LCDPI_MSG="pause LEDs."
-    fi
-  fi
-  CALL_LCDPI     
+  #CALL_LCDPI     
 else
   echo "ledsync.txt not found."    
 fi

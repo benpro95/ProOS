520fdc518de6037937d9d14f8d731048392c60e3
Ben Provenzano III <benprovenzano@gmail.com>
2024-01-14 13:52:11 -0500

    2024-01-14 13:52

--- rpi/config/preinit.sh
+++ rpi/config/preinit.sh
@@ -5,6 +5,12 @@
 ### Set CPU clock speed scaling
 echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
+### Setup Website System Log File
+mkdir -p /var/www/html/ram
+/opt/rpi/main-www server clearlog
+chmod -R 777 /var/www/html/ram/*
+chown -R www-data:www-data /var/www/html/ram/*
+
 ### Start network configuration website / automation API
 systemctl --no-block start lighttpd.service
 

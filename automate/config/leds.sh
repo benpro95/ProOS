be1a1ba6a90f6f538a5f9486e3cab5f2dc32cc21
Ben Provenzano III <benprovenzano@gmail.com>
2024-02-07 04:44:22 -0500

    2024-02-07 04:44

--- automate/config/leds.sh
+++ automate/config/leds.sh
@@ -5,21 +5,30 @@
 
 CALL_LCDPI(){
   ## Send message to LCDpi
+  LCD_HOST="lcdpi.home"
   if [ "$VARA" == "fc" ]; then
     LCDPI_MSG="$VARB% brightness."
   else
     LCDPI_MSG="$VARA."
     if [ "$VARA" == "stop" ]; then
-      LCDPI_MSG="stop LEDs."
+      LCDPI_MSG="stopped LEDs."
     fi
     if [ "$VARA" == "pause" ]; then
-      LCDPI_MSG="pause LEDs."
+      LCDPI_MSG="paused LEDs."
     fi
   fi
+  ## Convert Text
+  JSONDATA=$(echo "$LCDPI_MSG" | jq -Rsc '. / "\n" - [""]')
+  RAWDATA=$(echo "$JSONDATA" | base64)
+  ## Send Message
   /usr/bin/curl -silent --fail --ipv4 --no-buffer \
      --max-time 5 --retry 1 --no-keepalive \
-     -X POST http://lcdpi.home/upload.php \
-     -H "Content-Type: text/plain" -d "$LCDPI_MSG"
+     -X POST "http://$LCD_HOST/update.php?file=message&action=update" \
+     -H "Content-Type", "text/plain" --data "$RAWDATA"
+  ## Display Message
+  /usr/bin/curl -silent --fail --ipv4 --no-buffer \
+     --max-time 5 --retry 1 --no-keepalive \
+     --data "var=&arg=message&action=main" "http://$LCD_HOST/exec.php"
 }
 
 ## Read Input Arguments
@@ -36,7 +45,7 @@ if [ -e /var/www/html/ram/mainmenu.txt ]; then
     HOST="${LINE[0]}"  
     STATE="${LINE[1]}"
     ## Append active hosts to array
-    if [ "$STATE" == "1" ]; then
+    if [ "$STATE" == "1" ] || [ "$VARA" == "stop" ] || [ "$VARA" == "pause" ]; then
       DATA+="${HOST}"
       DATA+="${NEWLINE}"
     fi  
@@ -47,8 +56,11 @@ if [ -e /var/www/html/ram/mainmenu.txt ]; then
       /usr/bin/curl --fail --ipv4 --no-buffer \
       --max-time 5 --retry 1 --no-keepalive \
        --data "var=$VARB&arg=$VARA&action=leds" http://%/exec.php
+    DATAECHO="sending $VARA:$VARB $DATA..."  
+    DATAECHO=$(echo $DATAECHO|tr -d '\n')
+    echo "$DATAECHO"
   fi
-  #CALL_LCDPI     
+  CALL_LCDPI     
 else
   echo "mainmenu.txt not found."    
 fi

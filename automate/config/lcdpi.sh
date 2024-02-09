beebf7eaeeec7c709c4f6e104dc7eb57d3af1ff2
Ben Provenzano III <benprovenzano@gmail.com>
2024-02-08 21:08:54 -0500

    2024-02-08 21:08

--- automate/config/lcdpi.sh
+++ automate/config/lcdpi.sh
@@ -3,14 +3,13 @@ LCDPI_MSG=$1
 ## Send message to LCDpi
 LCD_HOST="lcdpi.home"
 
-## Convert Text
-JSONDATA=$(echo "$LCDPI_MSG" | jq -Rsc '. / "\n" - [""]')
-RAWDATA=$(echo "$JSONDATA" | base64)
+## Convert Text (TEXT->JSON->BASE64)
+JSONDATA=$(echo "$LCDPI_MSG" | jq -Rsc '. / "\n" - [""]' | base64)
 ## Send Message
 /usr/bin/curl -silent --fail --ipv4 --no-buffer \
    --max-time 5 --retry 2 --no-keepalive \
    -X POST "http://$LCD_HOST/update.php?file=message&action=update" \
-   -H "Content-Type", "text/plain" --data "$RAWDATA"
+   -H "Content-Type", "text/plain" --data "$JSONDATA"
 ## Display Message
 /usr/bin/curl -silent --fail --ipv4 --no-buffer \
    --max-time 5 --retry 2 --no-keepalive \

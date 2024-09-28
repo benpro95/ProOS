d36481b641ed08c50627d48454ed0ab844d95232
Ben Provenzano III <benprovenzano@gmail.com>
2024-09-27 23:09:34 -0400

    2024-09-27 23:09

--- files/config/savebookmarks.sh
+++ files/config/savebookmarks.sh
@@ -2,18 +2,19 @@
 ## Save bookmarks file from RAM disk hourly
 
 echo "(savebookmarks) started."
+DS_PATH="/mnt/data/Ben"
 
 if [ -f /mnt/ramdisk/bookmarks.txt ]; then
   echo "saving bookmarks to disk..."
-  mv -f /mnt/data/Documents/Bookmarks.txt /mnt/data/Documents/.Bookmarks.bak
-  rsync -a /mnt/ramdisk/bookmarks.txt /mnt/data/Documents/Bookmarks.txt
-  chmod 644 /mnt/data/Documents/Bookmarks.txt
-  chown ben:shared /mnt/data/Documents/Bookmarks.txt
+  mv -f $DS_PATH/Bookmarks.txt $DS_PATH/.Bookmarks.bak
+  rsync -a /mnt/ramdisk/bookmarks.txt $DS_PATH/Bookmarks.txt
+  chmod 644 $DS_PATH/Bookmarks.txt
+  chown ben:shared $DS_PATH/Bookmarks.txt
 else
   echo "bookmarks file not found in RAM disk, restoring..."
   if [ -f /mnt/ramdisk/drives.txt ]; then ## check if RAM disk exists
-  	if [ -f /mnt/data/Documents/Bookmarks.txt ]; then 
-	  cp -fv /mnt/data/Documents/Bookmarks.txt /mnt/ramdisk/bookmarks.txt
+  	if [ -f $DS_PATH/Bookmarks.txt ]; then 
+	  cp -fv $DS_PATH/Bookmarks.txt /mnt/ramdisk/bookmarks.txt
 	  chmod 777 /mnt/ramdisk/bookmarks.txt
 	  chown ben:shared /mnt/ramdisk/bookmarks.txt
 	  echo "Restore complete."

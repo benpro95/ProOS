c65a8bb9a04537f64dccde8e990facb6432d1452
Ben Provenzano III <benprovenzano@gmail.com>
2025-05-18 18:22:38 -0400

    2025-05-18 18:22

---
+++ automate/wkst/debian/installer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Update Sources
+apt-get --yes update
+
+## Install Packages
+apt-get install -y --no-upgrade --ignore-missing unzip wget \
+  rsync curl screen ethtool sshpass bc locales neofetch \
+  apt-transport-https nmap bpytop binutils iperf3 sshfs

5df8f9ed3ef8a5a001c4d8a724aa9e7b15628e77
Ben Provenzano III <benprovenzano@gmail.com>
2025-07-13 21:42:10 -0400

    2025-07-13 21:42

---
+++ wkst/kubuntu.bat
@@ -0,0 +1,3 @@
+cd "C:\Program Files (x86)\VMware\VMware Workstation\"
+START /B "VM" "C:\Program Files (x86)\VMware\VMware Workstation\vmware.exe" -n -x "D:\VMware\Kubuntu\Kubuntu.vmx"
+exit
--- wkst/mint.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Program Files (x86)\VMware\VMware Workstation\"
-START /B "VM" "C:\Program Files (x86)\VMware\VMware Workstation\vmware.exe" -n -x "D:\VMware\Linux Mint\Linux Mint.vmx"
-exit

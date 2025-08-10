50ba330a92cbde476719495c3746087ba37029c4
Ben Provenzano III <benprovenzano@gmail.com>
2025-08-09 21:05:32 -0400

    2025-08-09 21:05

--- automate/config/firmware/WindowLamp/main.py
+++ automate/config/firmware/WindowLamp/main.py
@@ -9,8 +9,85 @@ from machine import Pin
 # Wi-Fi credentials
 from wifi_creds import ssid, password
 
-# Create an LED object on pin 'LED'
-led = Pin(21, Pin.OUT)
+def sendResponse(data):
+    return '|' + str(data) + '|'
+
+def webServer(socket):
+    try:
+        response = 'X'
+        conn, addr = socket.accept()
+        print('Got a connection from', addr)
+        
+        # Receive and parse the request
+        request = conn.recv(1024)
+        request = str(request)
+        print('Request content = %s' % request)
+        try:
+            request = request.split()[1]
+            print('Request:', request)
+        except IndexError:
+            pass
+        
+        # Process the request
+        if request == '/led1on?':
+            print("turning LED #1 on")
+            led1.value(0) ## active-low
+            response = sendResponse(1);   
+            
+        elif request == '/led1off?':
+            print("turning LED #1 off")
+            led1.value(1)
+            response = sendResponse(0);   
+            
+        elif request == '/led1?':
+            print("LED #1 status")
+            if led1.value() == 0: ## active-low
+              response = sendResponse(1);
+            else:
+              response = sendResponse(0);  
+
+        elif request == '/led1toggle?':
+            print("toggling LED #1 power")
+            if led1.value() == 1:
+                led1.value(0)  ## active-low
+                response = sendResponse(1);
+            else:
+                led1.value(1)
+                response = sendResponse(0);
+        
+            
+            
+        # Send the HTTP response and close the connection
+        conn.send('HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\n')
+        conn.send(response + '\n')
+        conn.close()
+        
+    except OSError as e:
+        conn.close()
+        print('Connection closed')
+
+def openSocket():
+    # Set up socket and start listening
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(addr)
+    s.listen()
+    print('Listening on', addr)
+    return s
+
+def waitForConnection():
+    print("Waiting for Wi-Fi connection...")
+    time.sleep(10)
+    retrys = retrys + 1
+    if retrys >= 10:
+        print("No connection found rebooting!")
+        time.sleep(1)
+        machine.reset() # Reboot
+
+# LED #1 pin
+led1 = Pin(21, Pin.OUT)
+led1.value(1) ## off = HIGH
 
 # Connect to WLAN
 wlan = network.WLAN(network.STA_IF)
@@ -18,67 +95,33 @@ wlan.active(True)
 wlan.connect(ssid, password)
 
 # Wait for Wi-Fi connection
-connection_timeout = 10
-while connection_timeout > 0:
+while True:
     if wlan.status() >= 3:
         break
-    connection_timeout -= 1
-    print('Waiting for Wi-Fi connection...')
-    time.sleep(1)
-
+    waitForConnection()
+    
 # Check if connection is successful
 if wlan.status() != 3:
     raise RuntimeError('Failed to establish a network connection')
+    time.sleep(1)
+    machine.reset() # Reboot
 else:
     print('Connection successful!')
     network_info = wlan.ifconfig()
     print('IP address:', network_info[0])
 
-# Set up socket and start listening
-addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-s = socket.socket()
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(addr)
-s.listen()
+## Open web socket
+socket = openSocket()
+retrys = 0
 
-print('Listening on', addr)
-
-
-# Main loop to listen for connections
+## Entry point
 while True:
-    try:
-        response = 'X'
-        conn, addr = s.accept()
-        print('Got a connection from', addr)
-        
-        # Receive and parse the request
-        request = conn.recv(1024)
-        request = str(request)
-        print('Request content = %s' % request)
+    if wlan.isconnected():
+        retrys = 0
+        webServer(socket)
+    else:
+        waitForConnection()
 
-        try:
-            request = request.split()[1]
-            print('Request:', request)
-        except IndexError:
-            pass
-        
-        # Process the request and update variables
-        if request == '/led1on?':
-            print("turning LED on")
-            led.value(0)
-            response = '|1|'
-        elif request == '/led1off?':
-            print("turning LED off")
-            led.value(1)
-            response = '|0|'
-
-        # Send the HTTP response and close the connection
-        conn.send('HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\n')
-        conn.send(response + '\n')
-        conn.close()
-
-    except OSError as e:
-        conn.close()
-        print('Connection closed')
-        
-        
+    
+    
+    

34b2c8416f58fed240e7f5b93ad15bba77402299
Ben P <80415150+benpro95@users.noreply.github.com>
2025-09-25 16:51:21 -0400

    2025-09-25 16:51

--- files/config/apicmds.sh
+++ files/config/apicmds.sh
@@ -253,8 +253,8 @@ fi
 if [[ $CMD == "netscan" ]]
 then
   echo "** network scan **"
-  nmap --unprivileged -v --open -PT 10.177.1.0/24
-  nmap --unprivileged -v --open -sn 10.177.1.0/24
+  ##nmap --unprivileged -v --open -PT 192.168.1.0/24
+  nmap --unprivileged -v --open -sn 192.168.1.0/24
   exit
 fi
 

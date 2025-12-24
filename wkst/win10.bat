a7796fe546c7fb345b49166da0556484b1e04ce3
Ben P <80415150+benpro95@users.noreply.github.com>
2025-12-24 15:00:34 -0500

    2025-12-24 15:00

--- wkst/win10.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Program Files (x86)\VMware\VMware Workstation\"
-START /B "VM" "C:\Program Files (x86)\VMware\VMware Workstation\vmware.exe" -n -x "D:\VMware\Windows 10\Windows 10 Pro.vmx"
-exit
